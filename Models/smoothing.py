54f215a15ddf3c8e60250f3abbb0bcf2dfe5f38d
patrickpynadath <patrickpynadath2022@u.northwestern.edu>
2022-09-06 14:22:37 -0500

    testing out using VAE as preprocessing step

--- Models/smoothing.py
+++ Models/smoothing.py
@@ -147,7 +147,6 @@ class SmoothVAE_PreProcess(Smooth):
         encoded = self.trained_VAE.encoder(x)
         z_mean, z_var = self.trained_VAE.q(encoded)
         z = self.trained_VAE.z(z_mean, z_var)
-        print(z.size())
         return self.base_classifier(z)
 
 
