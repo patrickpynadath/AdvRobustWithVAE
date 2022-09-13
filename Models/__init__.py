9645997b43ef104aa5d96d0836956c06971b6355
patrickpynadath <patrickpynadath2022@u.northwestern.edu>
2022-09-13 12:57:34 -0500

    adding test for manifold experiments -- pixelcnn tests

--- Models/__init__.py
+++ Models/__init__.py
@@ -1,4 +1,4 @@
 from Models.classifiers import *
 from Models.smoothing import *
 from Models.vae import *
-from PixelCNN import PixelCNN, discretized_mix_logistic_loss
+from Models.PixelCNN import PixelCNN, discretized_mix_logistic_loss
