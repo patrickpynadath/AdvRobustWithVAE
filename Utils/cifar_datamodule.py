5e1417e6590bb153672058abe404f9ca83dfef08
patrickpynadath <patrickpynadath2022@u.northwestern.edu>
2022-09-20 15:15:05 -0500

    converted resnet_cifar to pytorch lightning

--- Utils/cifar_datamodule.py
+++ Utils/cifar_datamodule.py
@@ -4,20 +4,21 @@ import pytorch_lightning as pl
 
 
 class Cifar10DataModule(pl.LightningDataModule):
-    def __init__(self, data_dir : str, batch_size : int):
+    def __init__(self, data_dir : str, batch_size : int, num_workers : int):
         super().__init__()
         self.data_dir = data_dir
         self.batch_size = batch_size
+        self.num_workers = num_workers
 
     def setup(self, stage = None):
         self.cifar_train, self.cifar_test = get_cifar_sets(self.data_dir)
 
 
     def train_dataloader(self) -> DataLoader:
-        return DataLoader(self.cifar_train, batch_size=self.batch_size, shuffle=True)
+        return DataLoader(self.cifar_train, batch_size=self.batch_size, shuffle=True, num_workers=self.num_workers)
 
     def val_dataloader(self) -> DataLoader:
         return self.test_dataloader()
 
     def test_dataloader(self) -> DataLoader:
-        return DataLoader(self.cifar_test, batch_size=self.batch_size, shuffle=False)
+        return DataLoader(self.cifar_test, batch_size=self.batch_size, shuffle=False, num_workers=self.num_workers)
--- main.py
+++ main.py
@@ -1,16 +1,16 @@
 from Models import LightningResnet
-from torch.utils.data import DataLoader
-from Utils import Cifar10DataModule, get_cifar_sets
+from Utils import Cifar10DataModule
 from pytorch_lightning import Trainer
 from pytorch_lightning.loggers import TensorBoardLogger
 
 BATCH_SIZE = 100
 MAX_EPOCHS = 100
+DATALOADER_WORKERS = 32
 
 if __name__ == '__main__':
     dir = '*/'
     logger = TensorBoardLogger(save_dir="lightning_logs", name="resnet_test", flush_secs = 30)
-    data_module = Cifar10DataModule(data_dir=dir, batch_size=BATCH_SIZE)
+    data_module = Cifar10DataModule(data_dir=dir, batch_size=BATCH_SIZE, num_workers=DATALOADER_WORKERS)
     trainer = Trainer(gpus=1, auto_select_gpus=True, fast_dev_run=False, enable_progress_bar=True, max_epochs=MAX_EPOCHS, logger=logger)
     resnet = LightningResnet(depth=20, num_classes=10, block_name='BasicBlock')
     trainer.fit(model=resnet, datamodule=data_module)
