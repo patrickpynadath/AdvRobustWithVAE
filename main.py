8ecbe4c9742ac2340c2163e3c17b9e1e0711173d
patrickpynadath <patrickpynadath2022@u.northwestern.edu>
2022-09-20 15:23:05 -0500

    converted resnet_cifar to pytorch lightning

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ if __name__ == '__main__':
     dir = '*/'
     logger = TensorBoardLogger(save_dir="lightning_logs", name="resnet_test", flush_secs = 30)
     data_module = Cifar10DataModule(data_dir=dir, batch_size=BATCH_SIZE, num_workers=DATALOADER_WORKERS)
-    trainer = Trainer(gpus=1, auto_select_gpus=True, fast_dev_run=False, enable_progress_bar=True, max_epochs=MAX_EPOCHS, logger=logger)
+    trainer = Trainer(gpus=2, auto_select_gpus=True, fast_dev_run=False, enable_progress_bar=True, max_epochs=MAX_EPOCHS, logger=logger)
     resnet = LightningResnet(depth=20, num_classes=10, block_name='BasicBlock')
     trainer.fit(model=resnet, datamodule=data_module)
     #trainer.test(datamodule=data_module)
