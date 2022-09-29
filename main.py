c2eb9dc29f925bc8b797accaf65b9125637dbc9d
patrickpynadath <patrickpynadath2022@u.northwestern.edu>
2022-09-29 18:55:11 -0500

    getting data on generative models

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ if __name__ == '__main__':
     exp = BaseExp(training_logdir=TRAIN_METRICS_DIR,
                   exp_logdir=HYPERPARAM_DIR,
                   device='cuda')
-    for model_name in ['lvae', 'iwae', 'logcosh_vae', 'vae', 'vq_vae']:
+    for model_name in ['iwae', 'logcosh_vae', 'vae', 'vq_vae']:
         exp.get_trained_vae(64, 150, model_name, latent_dim=100,in_channels=3)
 
 
