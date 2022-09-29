794aa9fa061546a65fccdc031e794b81bc67acfe
patrickpynadath <patrickpynadath2022@u.northwestern.edu>
2022-09-29 16:42:22 -0500

    added files from really good repository for torch implementations of various vae archs, credit in the VAE_model init file

--- Models/VAE_Models/__init__.py
+++ Models/VAE_Models/__init__.py
@@ -1,1 +1,11 @@
 from Models.VAE_Models.models import *
+
+# citation for package -- all I did was make compatible with the existing framework I had
+# @misc{Subramanian2020,
+#   author = {Subramanian, A.K},
+#   title = {PyTorch-VAE},
+#   year = {2020},
+#   publisher = {GitHub},
+#   journal = {GitHub repository},
+#   howpublished = {\url{https://github.com/AntixK/PyTorch-VAE}}
+# }
