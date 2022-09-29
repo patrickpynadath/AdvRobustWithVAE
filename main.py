f555386de8eab6cef550c4ff1f915642adfc682c
patrickpynadath <patrickpynadath2022@u.northwestern.edu>
2022-09-29 18:47:14 -0500

    getting data on generative models

--- main.py
+++ main.py
@@ -7,12 +7,10 @@ TRAIN_METRICS_DIR = '../ExperimentLogging/TrainMetrics/'
 HYPERPARAM_DIR = '../ExperimentLogging/HyperParamMetrics/'
 
 if __name__ == '__main__':
-    # exp = BaseExp(training_logdir=TRAIN_METRICS_DIR,
-    #               exp_logdir=HYPERPARAM_DIR,
-    #               device='cuda')
-    # for model_name in ['vae', 'vq_vae']:
-    #     exp.get_trained_vae(64, 150, model_name, latent_dim=100,in_channels=3)
-    conf = Configuration()
-    autoencoder = AutoEncoder('cpu', conf)
-    autoencoder.load_state_dict(torch.load("saved_models/model_ema_norm_he-et-al.pth"))
+    exp = BaseExp(training_logdir=TRAIN_METRICS_DIR,
+                  exp_logdir=HYPERPARAM_DIR,
+                  device='cuda')
+    for model_name in ['betatc_vae', 'lvae', 'iwae', 'logcosh_vae', 'vae', 'vq_vae']:
+        exp.get_trained_vae(64, 150, model_name, latent_dim=100,in_channels=3)
+
 
