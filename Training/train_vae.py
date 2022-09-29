8162dd32012b470971d5e666645a1c142c8b9cf0
patrickpynadath <patrickpynadath2022@u.northwestern.edu>
2022-09-29 17:13:04 -0500

    fixing sampling of images for vae trainer

--- Training/train_vae.py
+++ Training/train_vae.py
@@ -139,11 +139,11 @@ class VAETrainer:
         assert mode in ['train', 'test', 'generate']
         if mode == 'train':
             batch, labels = next(iter(self.train_loader))
-            sampled_imgs = self.model.generate(batch)
+            sampled_imgs = self.model.generate(batch.to(self.device))
             return sampled_imgs
         elif mode == 'test':
             batch, labels = next(iter(self.test_loader))
-            sampled_imgs = self.model.generate(batch)
+            sampled_imgs = self.model.generate(batch.to(self.device))
             return sampled_imgs
         elif mode == 'generate':
             return self.model.sample(num_samples=self.batch_size, current_device=self.device)
