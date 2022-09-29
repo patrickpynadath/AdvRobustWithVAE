14a61a2b2434c83fc97631bb4e1d843756065340
patrickpynadath <patrickpynadath2022@u.northwestern.edu>
2022-09-29 18:36:19 -0500

    getting data on generative models

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from Experiments import run_hyperparam_clf, sanity_check, BaseExp
 from Models import Configuration, AutoEncoder
 import torch
+import os
 # global experiment variables that stay constant for every experiment
 # logistical parameters
 TRAIN_METRICS_DIR = '../ExperimentLogging/TrainMetrics/'
@@ -12,6 +13,7 @@ if __name__ == '__main__':
     #               device='cuda')
     # for model_name in ['vae', 'vq_vae']:
     #     exp.get_trained_vae(64, 150, model_name, latent_dim=100,in_channels=3)
+    print(os.curdir)
     conf = Configuration()
     autoencoder = AutoEncoder('cpu', conf)
     autoencoder.load_state_dict(torch.load("/saved_models/model_ema_norm_he-et-al.pth"))
