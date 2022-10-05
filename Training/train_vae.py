395a9b1d22c75d199f8ed3f6f7bfada6d8542b9c
patrickpynadath <patrickpynadath2022@u.northwestern.edu>
2022-10-05 17:34:54 -0500

    found good implementation of vq-vae, just trying to see if it works

--- Training/train_vae.py
+++ Training/train_vae.py
@@ -41,6 +41,7 @@ class VQVAETrainer:
 
     def training_step(self, batch):
         inputs, _ = batch
+        print(batch)
         inputs = inputs.to(self.device)
         self.optimizer.zero_grad()
 
