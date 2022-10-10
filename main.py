fixed img normalization

main.py
@@ -3,6 +3,7 @@ from Models import Configuration, AutoEncoder
 import torch
 from Experiments import train_save_necessary_models
 from Training import get_trained_vq_vae
+from Experiments import run_adv_robust
 # global experiment variables that stay constant for every experiment
 # logistical parameters
 TRAIN_METRICS_DIR = '../ExperimentLogging/TrainMetrics/'
@@ -10,10 +11,11 @@ HYPERPARAM_DIR = '../ExperimentLogging/HyperParamMetrics/'
 VAE_ADV_EXP = '../ExperimentLogging/AdversarialExpVAE/'
 ADV_ROB_EXP = '../ExperimentLogging/AdvRobExp/'
 if __name__ == '__main__':
-    exp = BaseExp(training_logdir=TRAIN_METRICS_DIR,
-                  exp_logdir=VAE_ADV_EXP,
-                  device='cuda')
-    vqvae_resnet = exp.get_vqvae_resnet()
-    torch.save(vqvae_resnet.state_dict(), 'saved_models/vqvae_resnet')
+    # exp = BaseExp(training_logdir=TRAIN_METRICS_DIR,
+    #               exp_logdir=VAE_ADV_EXP,
+    #               device='cuda')
+    # vqvae_resnet = exp.get_vqvae_resnet()
+    # torch.save(vqvae_resnet.state_dict(), 'saved_models/vqvae_resnet')
     # vq_vae = get_trained_vq_vae(TRAIN_METRICS_DIR, 15000)
     # torch.save(vq_vae.state_dict(), 'saved_models/vq_vae')
+    run_adv_robust()