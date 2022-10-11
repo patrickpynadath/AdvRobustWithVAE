6472367c662e0ededb51531d6ae17dc8dbbdf7ab
patrickpynadath <patrickpynadath2022@u.northwestern.edu>
2022-10-11 18:57:34 -0500

    running prelim adv rob exp

--- Experiments/adv_robust_prelim.py
+++ Experiments/adv_robust_prelim.py
@@ -12,6 +12,9 @@ def run_adv_robust():
     device = 'cuda'
     resnet_base = get_base_resnet(device)
     vqvae_clf = get_vqvae_clf(device)
+    vae_clf = get_vae_clf(device)
+    smoothing_sigmas = [.1, .25, .5]
+    smooth_models = get_resnet_smooth(device, smoothing_sigmas, resnet_base)
 
     _, test_dataset = get_cifar_sets()
     test_loader = DataLoader(test_dataset, batch_size=128)
@@ -19,11 +22,14 @@ def run_adv_robust():
 
     linf_eps = [1/255, 2/255, 5/255, 10/255]
     l2_eps = [.5, 1, 1.5, 2]
-    smoothing_sigmas = [.1, .25, .5]
 
-    models = {'base_clf': resnet_base,
+    models = {'Base-Resnet': resnet_base,
               'VQVAE-Resnet(ensemble)': vqvae_clf,
-              'VQVAE-Resnet(clf)': vqvae_clf.base_classifier}
+              'VQVAE-Resnet(clf)': vqvae_clf.base_classifier,
+              'VAE-Resnet(ensemble)' : vae_clf,
+              'VAE-Resnet(clf)' : vae_clf.base_classifier}
+    for idx, model in enumerate(smooth_models):
+        models[f'Smooth-Resnet-{smoothing_sigmas[idx]}'] = model
 
     total_res = {}
     for name in models.keys():
@@ -44,16 +50,17 @@ def run_adv_robust():
             num_correct = get_num_correct(pred, labels)
             total_res[model_name]['nat acc'] += num_correct/total_samples
 
-            # linf adversaries
+            # adversaries
             for attacker_type in attackers.keys():
                 norms = norm_lists[attacker_type]
                 for i in range(len(norms)):
 
                     if model_name == 'VQVAE-Resnet(ensemble)':
                         attacker = attackers[attacker_type](vqvae_clf.base_classifier, eps = norms[i], steps = 40)
+                    elif model_name == 'VAE-Resnet(ensemble)':
+                        attacker = attackers[attacker_type](vae_clf.base_classifier, eps = norms[i], steps = 40)
                     else:
-                        attacker = attackers[attacker_type](model, eps = norms[i], steps = 40)
-
+                        attacker = attackers[attacker_type](model, eps=norms[i], steps=40)
                     attacked_data = attacker(data, labels)
                     outputs = model(attacked_data)
                     pred = torch.argmax(outputs, dim=1)
@@ -80,6 +87,9 @@ def get_vae_clf(device):
     VAE = vae_models['VanillaVAE']
     vae = VAE(in_channels=3, latent_dim=512).to(device)
     vae.load_state_dict(torch.load('saved_models/vae_vanilla_base'))
+    resnet_base_clf = ResNet(depth=110, block_name='BottleNeck', num_classes=10).to(device)
+    resnet_base_clf.load_state_dict(torch.load('saved_models/resnet_vae_ensemble'))
+    return VAE_CLF(base_classifier=resnet_base_clf, vae=VAE)
 
 
 def get_vqvae_clf(device):
@@ -98,3 +108,16 @@ def get_vqvae_clf(device):
                    commitment_cost).to(device)
     vq_vae.load_state_dict(torch.load('saved_models/vqvae_ensemble'))
     return VQVAE_CLF(base_resnet_clf, vq_vae)
+
+
+# for right  now, the resnet is NOT trained with smoothing -- so for right now, each of the smooth models
+# should be fine with just pointer access to the model
+def get_resnet_smooth(device,
+                      smoothing_sigmas,
+                      resnet,
+                      m_train = 10):
+    models = []
+    for sigma in smoothing_sigmas:
+        clf = Smooth(resnet, sigma, device, m_train, 10)
+        models.append(clf)
+    return models
