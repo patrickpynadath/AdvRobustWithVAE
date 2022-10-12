def1841bdfefce9660e2b163b15aaeca90432a87
patrickpynadath <patrickpynadath2022@u.northwestern.edu>
2022-10-12 07:06:37 -0500

    running prelim adv rob exp

--- Experiments/adv_robust_prelim.py
+++ Experiments/adv_robust_prelim.py
@@ -7,78 +7,268 @@ from torchattacks import PGD, PGDL2
 from tqdm import tqdm
 import pickle
 
+device = 'cuda'
 
-def run_adv_robust():
-    device = 'cuda'
+
+def test_resnet_base(test_loader, attackers, norm_lists, acc_dct):
     resnet_base = get_base_resnet(device)
     resnet_base.eval()
+    total = 0
+    progress_bar = tqdm(enumerate(test_loader), total=len(test_loader))
+    for batch_idx, batch in progress_bar:
+        data, labels = batch
+        data = data.to(device)
+        outputs = resnet_base(data)
+        pred = torch.argmax(outputs, dim=1)
+        num_correct = get_num_correct(pred, labels)
+        acc_dct['Base-Resnet']['nat acc'] += num_correct
+        total += len(data)
+    acc_dct['Base-Resnet']['nat acc'] /= total
+    print(f"Nat Acc: {acc_dct['Base-Resnet']['nat acc']}")
+    for attacker_type in attackers.keys():
+        print(attacker_type)
+        norms = norm_lists[attacker_type]
+        for i in range(len(norms)):
+            torch.cuda.empty_cache()
+            print(f"Eps: {round(norms[i], 5)}")
+            attacker = attackers[attacker_type](resnet_base, eps=norms[i], steps=20)
+            progress_bar = tqdm(enumerate(test_loader), total=len(test_loader))
+            total = 0
+            for batch_idx, batch in progress_bar:
+                data, labels = batch
+                attacked_data = attacker(data, labels)
+                outputs = resnet_base(attacked_data)
+                pred = torch.argmax(outputs, dim=1)
+                acc_dct['Base-Resnet'][attacker_type][i] += get_num_correct(pred, labels)
+                total += len(data)
+            acc_dct['Base-Resnet'][attacker_type][i] /= total
+            print(f"Adv Acc: {acc_dct['Base-Resnet'][attacker_type][i]}")
+    print(f"Done testing {'Base-Resnet'}")
+    return
+
+
+def test_vq_vae_ensemble(test_loader, attackers, norm_lists, acc_dct):
     vqvae_clf = get_vqvae_clf(device)
     vqvae_clf.eval()
+    progress_bar = tqdm(enumerate(test_loader), total=len(test_loader))
+    total = 0
+    for batch_idx, batch in progress_bar:
+        data, labels = batch
+        data = data.to(device)
+        outputs = vqvae_clf(data)
+        pred = torch.argmax(outputs, dim=1)
+        num_correct = get_num_correct(pred, labels)
+        acc_dct['VQVAE-Resnet(ensemble)']['nat acc'] += num_correct
+        total += len(data)
+    acc_dct['VQVAE-Resnet(ensemble)']['nat acc'] /= total
+    print(f"Nat Acc: {acc_dct['VQVAE-Resnet(ensemble)']['nat acc']}")
+    for attacker_type in attackers.keys():
+        print(attacker_type)
+        norms = norm_lists[attacker_type]
+        for i in range(len(norms)):
+            torch.cuda.empty_cache()
+            print(f"Eps: {round(norms[i], 5)}")
+            attacker = attackers[attacker_type](vqvae_clf.base_classifier, eps=norms[i], steps=20)
+            progress_bar = tqdm(enumerate(test_loader), total=len(test_loader))
+            total = 0
+            for batch_idx, batch in progress_bar:
+                data, labels = batch
+                attacked_data = attacker(data, labels)
+                outputs = vqvae_clf(attacked_data)
+                pred = torch.argmax(outputs, dim=1)
+                acc_dct['VQVAE-Resnet(ensemble)'][attacker_type][i] += get_num_correct(pred, labels)
+                total += len(data)
+            acc_dct['VQVAE-Resnet(ensemble)'][attacker_type][i] /= total
+            print(f"Adv Acc: {acc_dct['VQVAE-Resnet(ensemble)'][attacker_type][i]}")
+    print(f"Done testing {'VQVAE-Resnet(ensemble)'}")
+    return
+
+
+def test_vq_vae_clf(test_loader, attackers, norm_lists, acc_dct):
+    vqvae_clf = get_vqvae_clf(device)
+    model = vqvae_clf.base_classifier
+    vqvae_clf.vq_vae.to('cpu')
+    model.eval()
+    progress_bar = tqdm(enumerate(test_loader), total=len(test_loader))
+    total = 0
+    for batch_idx, batch in progress_bar:
+        data, labels = batch
+        data = data.to(device)
+        outputs = model(data)
+        pred = torch.argmax(outputs, dim=1)
+        num_correct = get_num_correct(pred, labels)
+        acc_dct['VQVAE-Resnet(clf)']['nat acc'] += num_correct
+        total += len(data)
+    acc_dct['VQVAE-Resnet(clf)']['nat acc'] /= total
+    print(f"Nat Acc: {acc_dct['VQVAE-Resnet(clf)']['nat acc']}")
+    for attacker_type in attackers.keys():
+        print(attacker_type)
+        norms = norm_lists[attacker_type]
+        for i in range(len(norms)):
+            torch.cuda.empty_cache()
+            print(f"Eps: {round(norms[i], 5)}")
+            attacker = attackers[attacker_type](model, eps=norms[i], steps=20)
+            progress_bar = tqdm(enumerate(test_loader), total=len(test_loader))
+            total = 0
+            for batch_idx, batch in progress_bar:
+                data, labels = batch
+                attacked_data = attacker(data, labels)
+                outputs = model(attacked_data)
+                pred = torch.argmax(outputs, dim=1)
+                acc_dct['VQVAE-Resnet(clf)'][attacker_type][i] += get_num_correct(pred, labels)
+                total += len(data)
+            acc_dct['VQVAE-Resnet(clf)'][attacker_type][i] /= total
+            print(f"Adv Acc: {acc_dct['VQVAE-Resnet(clf)'][attacker_type][i]}")
+    print(f"Done testing {'VQVAE-Resnet(clf)'}")
+    return
+
+
+def test_vae_ensemble(test_loader, attackers, norm_lists, acc_dct):
     vae_clf = get_vae_clf(device)
     vae_clf.eval()
+    progress_bar = tqdm(enumerate(test_loader), total=len(test_loader))
+    total = 0
+    for batch_idx, batch in progress_bar:
+        data, labels = batch
+        data = data.to(device)
+        outputs = vae_clf(data)
+        pred = torch.argmax(outputs, dim=1)
+        num_correct = get_num_correct(pred, labels)
+        acc_dct['VAE-Resnet(ensemble)']['nat acc'] += num_correct
+        total += len(data)
+    acc_dct['VAE-Resnet(ensemble)']['nat acc'] /= total
+    print(f"Nat Acc: {acc_dct['VAE-Resnet(ensemble)']['nat acc']}")
+    for attacker_type in attackers.keys():
+        print(attacker_type)
+        norms = norm_lists[attacker_type]
+        for i in range(len(norms)):
+            torch.cuda.empty_cache()
+            print(f"Eps: {round(norms[i], 5)}")
+            attacker = attackers[attacker_type](vae_clf.base_classifier, eps=norms[i], steps=20)
+            progress_bar = tqdm(enumerate(test_loader), total=len(test_loader))
+            total = 0
+            for batch_idx, batch in progress_bar:
+                data, labels = batch
+                attacked_data = attacker(data, labels)
+                outputs = vae_clf(attacked_data)
+                pred = torch.argmax(outputs, dim=1)
+                acc_dct['VAE-Resnet(ensemble)'][attacker_type][i] += get_num_correct(pred, labels)
+                total += len(data)
+            acc_dct['VAE-Resnet(ensemble)'][attacker_type][i] /= total
+            print(f"Adv Acc: {acc_dct['VAE-Resnet(ensemble)'][attacker_type][i]}")
+    print(f"Done testing {'VAE-Resnet(ensemble)'}")
+    return
+
+
+def test_vae_clf(test_loader, attackers, norm_lists, acc_dct):
+    vae_clf = get_vae_clf(device)
+    model = vae_clf.base_classifier
+    vae_clf.vae.to('cpu')
+    model.eval()
+    progress_bar = tqdm(enumerate(test_loader), total=len(test_loader))
+    total = 0
+    for batch_idx, batch in progress_bar:
+        data, labels = batch
+        data = data.to(device)
+        outputs = model(data)
+        pred = torch.argmax(outputs, dim=1)
+        num_correct = get_num_correct(pred, labels)
+        acc_dct['VAE-Resnet(clf)']['nat acc'] += num_correct
+        total += len(data)
+    acc_dct['VAE-Resnet(clf)']['nat acc'] /= total
+    print(f"Nat Acc: {acc_dct['VAE-Resnet(clf)']['nat acc']}")
+    for attacker_type in attackers.keys():
+        print(attacker_type)
+        norms = norm_lists[attacker_type]
+        for i in range(len(norms)):
+            torch.cuda.empty_cache()
+            print(f"Eps: {round(norms[i], 5)}")
+            attacker = attackers[attacker_type](model, eps=norms[i], steps=20)
+            progress_bar = tqdm(enumerate(test_loader), total=len(test_loader))
+            total = 0
+            for batch_idx, batch in progress_bar:
+                data, labels = batch
+                attacked_data = attacker(data, labels)
+                outputs = model(attacked_data)
+                pred = torch.argmax(outputs, dim=1)
+                acc_dct['VAE-Resnet(clf)'][attacker_type][i] += get_num_correct(pred, labels)
+                total += len(data)
+            acc_dct['VAE-Resnet(clf)'][attacker_type][i] /= total
+            print(f"Adv Acc: {acc_dct['VAE-Resnet(clf)'][attacker_type][i]}")
+    print(f"Done testing {'VAE-Resnet(clf)'}")
+    return
+
+
+def test_smooth_resnet(test_loader, attackers, norm_lists, acc_dct, sigma):
+    model = get_resnet_smooth(device, sigma)
+    model.eval()
+    progress_bar = tqdm(enumerate(test_loader), total=len(test_loader))
+    total = 0
+    for batch_idx, batch in progress_bar:
+        data, labels = batch
+        data = data.to(device)
+        outputs = model(data)
+        pred = torch.argmax(outputs, dim=1)
+        num_correct = get_num_correct(pred, labels)
+        acc_dct[f'Smooth-Resnet-{sigma}']['nat acc'] += num_correct
+        total += len(data)
+    acc_dct[f'Smooth-Resnet-{sigma}']['nat acc'] /= total
+    print(f"Nat Acc: {acc_dct[f'Smooth-Resnet-{sigma}']['nat acc']}")
+    for attacker_type in attackers.keys():
+        print(attacker_type)
+        norms = norm_lists[attacker_type]
+        for i in range(len(norms)):
+            torch.cuda.empty_cache()
+            print(f"Eps: {round(norms[i], 5)}")
+            attacker = attackers[attacker_type](model.base_classifier, eps=norms[i], steps=20)
+            progress_bar = tqdm(enumerate(test_loader), total=len(test_loader))
+            total = 0
+            for batch_idx, batch in progress_bar:
+                data, labels = batch
+                attacked_data = attacker(data, labels)
+                outputs = model(attacked_data)
+                pred = torch.argmax(outputs, dim=1)
+                acc_dct[f'Smooth-Resnet-{sigma}'][attacker_type][i] += get_num_correct(pred, labels)
+                total += len(data)
+            acc_dct[f'Smooth-Resnet-{sigma}'][attacker_type][i] /= total
+            print(f"Adv Acc: {acc_dct[f'Smooth-Resnet-{sigma}'][attacker_type][i]}")
+    print(f"Done testing {f'Smooth-Resnet-{sigma}'}")
+    return
+
+
+def run_adv_robust():
+
     smoothing_sigmas = [.1, .25, .5]
-    smooth_models = get_resnet_smooth(device, smoothing_sigmas, resnet_base)
 
     _, test_dataset = get_cifar_sets()
     test_loader = DataLoader(test_dataset, batch_size=128, shuffle=False)
-    total_samples = len(test_dataset)
 
     linf_eps = [1/255, 2/255, 5/255, 10/255]
     l2_eps = [.5, 1, 1.5, 2]
-
-    models = {'Base-Resnet': resnet_base,
-              'VQVAE-Resnet(ensemble)': vqvae_clf,
-              'VQVAE-Resnet(clf)': vqvae_clf.base_classifier,
-              'VAE-Resnet(ensemble)' : vae_clf,
-              'VAE-Resnet(clf)' : vae_clf.base_classifier}
-    for idx, model in enumerate(smooth_models):
-        models[f'Smooth-Resnet-{smoothing_sigmas[idx]}'] = model
+    models = ['Base-Resnet', 'VQVAE-Resnet(ensemble)', 'VQVAE-Resnet(clf)', 'VAE-Resnet(ensemble)', 'VAE-Resnet(clf)']
+    for sigma in smoothing_sigmas:
+        models.append(f'Smooth-Resnet-{sigma}')
 
     total_res = {}
-    for name in models.keys():
+    for name in models:
         total_res[name] = {'nat acc': 0,
                            'linf adv': [0 for _ in range(len(linf_eps))],
                            'l2 adv': [0 for _ in range(len(l2_eps))]}
     attackers = {'linf adv': PGD, 'l2 adv': PGDL2}
     norm_lists = {'linf adv': linf_eps, 'l2 adv': l2_eps}
-
-    for model_name in models.keys():
-        model = models[model_name]
-        print(f"testing {model_name}")
-        print('Testing Nat Acc')
-        progress_bar = tqdm(enumerate(test_loader), total=len(test_loader))
-        total = 0
-        for batch_idx, batch in progress_bar:
-            data, labels = batch
-            data = data.to(device)
-            outputs = model(data)
-            pred = torch.argmax(outputs, dim=1)
-            num_correct = get_num_correct(pred, labels)
-            total_res[model_name]['nat acc'] += num_correct
-            total += len(data)
-        total_res[model_name]['nat acc'] /= total
-        print(f"Nat Acc: {total_res[model_name]['nat acc']}")
-        for attacker_type in attackers.keys():
-            print(attacker_type)
-            norms = norm_lists[attacker_type]
-            for i in range(len(norms)):
-                print(f"Eps: {round(norms[i], 5)}")
-                if model_name == 'VQVAE-Resnet(ensemble)':
-                    attacker = attackers[attacker_type](vqvae_clf.base_classifier, eps=norms[i], steps=40)
-                elif model_name == 'VAE-Resnet(ensemble)':
-                    attacker = attackers[attacker_type](vae_clf.base_classifier, eps=norms[i], steps=40)
-                else:
-                    attacker = attackers[attacker_type](model, eps=norms[i], steps=20)
-                progress_bar = tqdm(enumerate(test_loader), total=len(test_loader))
-                for batch_idx, batch in progress_bar:
-                    data, labels = batch
-                    attacked_data = attacker(data, labels)
-                    outputs = model(attacked_data)
-                    pred = torch.argmax(outputs, dim=1)
-                    total_res[model_name][attacker_type][i] += get_num_correct(pred, labels)/total_samples
-                print(f"Adv Acc: {total_res[model_name][attacker_type][i]}")
-        print(f"Done testing {model_name}")
-        print("")
+    test_resnet_base(test_loader, attackers, norm_lists, total_res)
+    torch.cuda.empty_cache()
+    for sigma in smoothing_sigmas:
+        test_smooth_resnet(test_loader, attackers, norm_lists, total_res, sigma)
+        torch.cuda.empty_cache()
+    test_vae_ensemble(test_loader, attackers, norm_lists, total_res)
+    torch.cuda.empty_cache()
+    test_vae_clf(test_loader, attackers, norm_lists, total_res)
+    torch.cuda.empty_cache()
+    test_vq_vae_ensemble(test_loader, attackers, norm_lists, total_res)
+    torch.cuda.empty_cache()
+    test_vq_vae_clf(test_loader, attackers, norm_lists, total_res)
+    torch.cuda.empty_cache()
     # saving the data
     with open(r'res/prelim_adv_res.pickle', 'wb') as output_file:
         pickle.dump(total_res, output_file)
@@ -126,11 +316,9 @@ def get_vqvae_clf(device):
 # for right  now, the resnet is NOT trained with smoothing -- so for right now, each of the smooth models
 # should be fine with just pointer access to the model
 def get_resnet_smooth(device,
-                      smoothing_sigmas,
-                      resnet,
+                      sigma,
                       m_train = 10):
-    models = []
-    for sigma in smoothing_sigmas:
-        clf = Smooth(resnet, sigma, device, m_train, 10)
-        models.append(clf)
-    return models
+    resnet = get_base_resnet(device)
+    clf = Smooth(resnet, sigma, device, m_train, 10)
+    return clf
+
