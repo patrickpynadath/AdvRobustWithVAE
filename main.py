04f91d2895e708c9d9b8ba7b0ab0d2d033fb9d1b
patrickpynadath <patrickpynadath2022@u.northwestern.edu>
2022-10-12 14:50:57 -0500

    temporarily copied torch attacks -- PGD and PGDL2 clamped values to 0, 1, which messes up comparisons

--- main.py
+++ main.py
@@ -11,15 +11,15 @@ HYPERPARAM_DIR = '../ExperimentLogging/HyperParamMetrics/'
 VAE_ADV_EXP = '../ExperimentLogging/AdversarialExpVAE/'
 ADV_ROB_EXP = '../ExperimentLogging/AdvRobExp/'
 if __name__ == '__main__':
-    vq_vae = get_trained_vq_vae(TRAIN_METRICS_DIR, 6000)
-    torch.save(vq_vae.state_dict(), 'saved_models/vq_vae_0_1')
-    torch.cuda.empty_cache()
-    exp = BaseExp(training_logdir=TRAIN_METRICS_DIR,
-                  exp_logdir=VAE_ADV_EXP,
-                  device='cuda')
-    vae = exp.get_trained_vanilla_vae(batch_size=256, epochs=150, vae_model='vae')
-    torch.save(vae.state_dict(), 'saved_models/vae_0_1')
+    # vq_vae = get_trained_vq_vae(TRAIN_METRICS_DIR, 6000)
+    # torch.save(vq_vae.state_dict(), 'saved_models/vq_vae_0_1')
+    # torch.cuda.empty_cache()
+    # exp = BaseExp(training_logdir=TRAIN_METRICS_DIR,
+    #               exp_logdir=VAE_ADV_EXP,
+    #               device='cuda')
+    # vae = exp.get_trained_vanilla_vae(batch_size=256, epochs=150, vae_model='vae')
+    # torch.save(vae.state_dict(), 'saved_models/vae_0_1')
     # torch.save(vae_clf.base_classifier.state_dict(), 'saved_models/resnet_vae_ensemble')
     # vq_vae = get_trained_vq_vae(TRAIN_METRICS_DIR, 15000)
     # torch.save(vq_vae.state_dict(), 'saved_models/vq_vae')
-    # run_adv_robust()
+    run_adv_robust()
