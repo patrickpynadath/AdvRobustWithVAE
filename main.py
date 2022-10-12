0c199e469e1844c8b458f870cc7ded9f25c8e35f
patrickpynadath <patrickpynadath2022@u.northwestern.edu>
2022-10-11 20:05:26 -0500

    running prelim adv rob exp

--- main.py
+++ main.py
@@ -11,11 +11,12 @@ HYPERPARAM_DIR = '../ExperimentLogging/HyperParamMetrics/'
 VAE_ADV_EXP = '../ExperimentLogging/AdversarialExpVAE/'
 ADV_ROB_EXP = '../ExperimentLogging/AdvRobExp/'
 if __name__ == '__main__':
-    exp = BaseExp(training_logdir=TRAIN_METRICS_DIR,
-                  exp_logdir=VAE_ADV_EXP,
-                  device='cuda')
-    vae_clf = exp.get_vae_resnet(net_depth=110, block_name='BottleNeck')
-    torch.save(vae_clf.base_classifier.state_dict(), 'saved_models/resnet_vae_ensemble')
+    run_adv_robust()
+    # exp = BaseExp(training_logdir=TRAIN_METRICS_DIR,
+    #               exp_logdir=VAE_ADV_EXP,
+    #               device='cuda')
+    # vae_clf = exp.get_vae_resnet(net_depth=110, block_name='BottleNeck')
+    # torch.save(vae_clf.base_classifier.state_dict(), 'saved_models/resnet_vae_ensemble')
     # vq_vae = get_trained_vq_vae(TRAIN_METRICS_DIR, 15000)
     # torch.save(vq_vae.state_dict(), 'saved_models/vq_vae')
     # run_adv_robust()
