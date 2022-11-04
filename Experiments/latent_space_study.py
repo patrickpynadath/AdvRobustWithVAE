27ef91eed531df91343733670b66d9d502433bcf
patrickpynadath <patrickpynadath2022@u.northwestern.edu>
2022-11-04 12:46:06 -0500

    added code for peturbation analysis

--- Experiments/latent_space_study.py
+++ Experiments/latent_space_study.py
@@ -60,6 +60,55 @@ def get_random_sample_latent_diffs(class_idx,
     return df
 
 
+def get_random_sample_recon_diffs(class_idx,
+                                   model_dct,
+                                   idx_dct,
+                                   device,
+                                   dataset,
+                                   num_samples=1000):
+    gen_models = ['ae', 'vae', 'vqvae']
+    norm_types = ['l2', 'linf']
+
+    norm_data_same = {}
+    norm_data_diff = {}
+    for t in norm_types:
+        dct_same = {}
+        dct_diff = {}
+        for m in gen_models:
+            dct_same[m] = []
+            dct_diff[m] = []
+        norm_data_same[t] = dct_same
+        norm_data_diff[t] = dct_diff
+
+    out_classes = [i for i in range(10)]
+    out_classes.remove(class_idx)
+    for sample_idx in range(num_samples):
+        [orig_sample, in_sample] = random.sample(idx_dct[class_idx], 2)
+        # out sample
+        out_class = random.sample(out_classes, 1)[0]
+        out_sample = random.sample(idx_dct[out_class], 1)[0]
+        to_reconstruct = torch.stack([dataset[idx][0] for idx in [orig_sample, in_sample, out_sample]]).to(device)
+
+        for m in gen_models:
+            gen_model = model_dct[m].to(device)
+            gen_model.eval()
+            codes = gen_model.generate(to_reconstruct)
+            differences_same_class_code = get_norm_comparison(codes[0, :] - codes[1, :], batch=False)
+            differences_diff_class_code = get_norm_comparison(codes[0, :] - codes[2, :], batch=False)
+
+            for t in norm_types:
+                norm_data_same[t][m].append(differences_same_class_code[t])
+                norm_data_diff[t][m].append(differences_diff_class_code[t])
+    res = {'same_class': norm_data_same, 'diff_class': norm_data_diff}
+    df = pd.DataFrame()
+    for k1 in res.keys():
+        for k2 in res[k1].keys():
+            for k3 in res[k1][k2].keys():
+                df[f"{k1}_{k2}_{k3}"] = res[k1][k2][k3]
+
+    return df
+
+
 def get_random_sample_orig_diffs(class_idx,
                                  idx_dct,
                                  device,
