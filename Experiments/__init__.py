853eca7027f0a73917b565fc758dcac1a4e9f850
patrickpynadath <patrickpynadath2022@u.northwestern.edu>
2022-11-29 16:05:07 -0600

    added norm constrained noise

--- Experiments/__init__.py
+++ Experiments/__init__.py
@@ -1,6 +1,6 @@
 from .base_exp import BaseExp
 from .helper_functions import train_models, load_models, get_untrained_models, get_latent_code_vae, \
-    get_latent_code_vqvae, get_latent_code_ae, NormConstrainedAttacker
+    get_latent_code_vqvae, get_latent_code_ae, NormConstrainedAttacker, get_norm_constrained_noise
 from .raw_adv_rob import run_raw_adv_rob
 from .latent_space_study import get_random_sample_latent_diffs, get_generative_outputs, peturbation_analysis, get_total_res_peturbation, get_class_comparisons, get_random_sample_orig_diffs, get_random_sample_recon_diffs
 latent_code_fn = {'ae' : get_latent_code_ae, 'vae' : get_latent_code_vae, 'vqvae': get_latent_code_vqvae}
