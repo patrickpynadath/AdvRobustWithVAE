fef9aff298373228942d1c46eeedcc97af712ee8
patrickpynadath <patrickpynadath2022@u.northwestern.edu>
2022-12-05 17:46:09 -0600

    attack for smooth soft classifier to simulate PGD on hard classifier

--- Experiments/base_exp.py
+++ Experiments/base_exp.py
@@ -78,7 +78,7 @@ class BaseExp:
         for i, data in pg:
             inputs, labels = data[0].to(self.device), data[1].to(self.device)
             for i in range(len(labels)):
-                pred = smooth.predict(inputs[i, :], num_samples, conf_value, num_samples)
+                pred = smooth.predict(inputs[i, :], num_samples, conf_value, batch_size=10)
                 total += 1
                 if pred == labels[i].item():
                     num_correct += 1
@@ -111,7 +111,7 @@ class BaseExp:
                 attacked_inputs = get_adv_examples(attack_model, attack_eps=eps, adversary_type=adversary_type,
                                                    steps=steps, nat_img=inputs, labels=labels)
                 for i in range(len(labels)):
-                    pred_class = smooth.predict(attacked_inputs[i, :], num_samples, conf_value, batch_size=num_samples)
+                    pred_class = smooth.predict(attacked_inputs[i, :], num_samples, conf_value, batch_size=10)
                     total += 1
                     if pred_class == labels[i].item():
                         num_correct += 1
