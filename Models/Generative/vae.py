e8c6a2023132d697aef365658710a65d8199868a
patrickpynadath <patrickpynadath2022@u.northwestern.edu>
2022-12-13 21:58:47 -0600

    taking care of some inefficient memory usage for sampling

--- Models/Generative/vae.py
+++ Models/Generative/vae.py
@@ -32,13 +32,13 @@ class VAE(nn.Module):
                                 num_residual_layers,
                                 num_residual_hiddens)
         self.label= f'VAE_{timestamp()}'
-        self.apply(self._init_weights)
+        #self.apply(self._init_weights)
         torch.nn.init.constant_(self._fc_var.weight.data, 0)
         torch.nn.init.constant_(self._fc_var.bias.data, 0)
 
     def _init_weights(self, module):
         if isinstance(module, nn.Linear):
-            torch.nn.init.uniform_(module.weight.data, -.08, .08)
+            torch.nn.init.uniform_(module.weight.data, -.01, .08)
             if module.bias is not None:
                 module.bias.data.zero_()
 
