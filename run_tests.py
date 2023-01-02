3dfba3846f1545d9ab3d1d99669f21b126c2da92
patrickpynadath <patrickpynadath2022@u.northwestern.edu>
2023-01-02 11:59:57 -0600

    deleted unused tests

--- run_tests.py
+++
@@ -1,6 +0,0 @@
-import unittest
-from UnitTests import Test_Training_VAE, Test_Training_SmoothVAE, TestConvVAE, Test_Training_ResNet, TestResNet, TestBaseExp
-
-if __name__ == '__main__':
-
-    unittest.main()
